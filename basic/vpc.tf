a6f1e25ea8512e3320c85f3e44b1b0297d76b041
raghu-kadali <raghu2.kadali@gmail.com>
2025-12-09 12:23:47 +0530

    Update vpc.tf

--- basic/vpc.tf
+++ basic/vpc.tf
@@ -1,50 +1,50 @@
 provider "google" {
-    project = "kubernetes-477004"
-    region = "us-central1"
-  
-}
-
-
-resource "google_compute_instance" "tf_vm1" {
-    name = "harness-vm"
-    zone = "us-central1-c"
-    machine_type = "e2-medium"
-    boot_disk {
-      initialize_params {
-        image = "ubuntu-os-cloud/ubuntu-2204-lts"
-      }
-    }
-    network_interface {
-        network = google_compute_network.tf_net1.id
-        subnetwork = google_compute_subnetwork.tf-subnet1.id
-      access_config {
-        
-      }
-    }
-    metadata_startup_script = file("startup-script.sh")
+  project = "kubernetes-477004"
+  region  = "us-central1"
 }
 
 resource "google_compute_network" "tf_net1" {
-    name = "harness-net"
-    auto_create_subnetworks = false
+  name                    = "harness-net"
+  auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "tf-subnet1" {
-    name = "harness-subnet"
-    network = google_compute_network.tf_net1.id
-    region = "us-central1"
-    ip_cidr_range = "10.0.0.0/24"
+resource "google_compute_subnetwork" "tf_subnet1" {
+  name          = "harness-subnet"
+  network       = google_compute_network.tf_net1.id
+  region        = "us-central1"
+  ip_cidr_range = "10.0.0.0/24"
+}
+
+resource "google_compute_instance" "tf_vm1" {
+  name         = "harness-vm"
+  zone         = "us-central1-c"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.tf_net1.id
+    subnetwork = google_compute_subnetwork.tf_subnet1.id
+
+    access_config {}
+  }
 }
 
 resource "google_compute_firewall" "tf_fir" {
-    name = "allow-ssh-http"
-    direction = "INGRESS"
-    network = google_compute_network.tf_net1.id
-    source_ranges = ["0.0.0.0/0"]
-    allow {
-      protocol = "tcp"  
-      ports = ["22","80"]
-    }
+  name      = "allow-ssh-http"
+  network   = google_compute_network.tf_net1.id
+  direction = "INGRESS"
+
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80"]
+  }
 }
 
 output "harness_vm_ip" {
